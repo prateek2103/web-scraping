a360ada0218c44b5506323af9f8b435ff03f7593
prateek2103 <prateek007.purohit@gmail.com>
2020-04-04 01:14:55 +0530

    minor changes and improved link of linkedin

--- job_aggregator/seeker.py
+++ job_aggregator/seeker.py
@@ -1,49 +1,55 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import urlopen,Request
 import csv
 import re
+import requests
 
-for start in range(0,1):
-    try:
-        #url="https://www.indeed.co.in/jobs?q=engineer&l=Delhi&start={}".format(start)
-        url= "https://in.linkedin.com/jobs/search?keywords=engineer&location=Delhi"
-        html_doc=urlopen(url).read()
-        soup=BeautifulSoup(html_doc,'html.parser')
-        
-        #to deal with encoding error
-        with open("x.html","w",encoding="utf-8") as f:
-            f.write(str(soup))
+urls=[{"url_name":"indeed","url":"https://www.indeed.co.in/jobs?q={}&l={}&start={}","class_tag":"div","class":"jobsearch-SerpJobCard unifiedRow row result clickcard","encoding":True,"start_range":10},
+      {"url_name":"linkedin","url":"https://in.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={}&location={}&start={}","class_tag":"li","class":"result-card job-result-card result-card--with-hover-state","encoding":False,"start_range":20},
+      ]
+      
+f1="engineer";f2="Delhi"
 
-        with open("x.html","r") as f:
-            with open("data.csv","a") as f2:
-                # writer=csv.writer(f2)
-                
-                # html=f.read()
-                # new_soup=BeautifulSoup(html,'html.parser')
-                #jobs=soup.findAll(class_=["jobsearch-SerpJobCard" ,"unifiedRow" ,"row","result","clickcard"])
-                jobs=soup.findAll(class_=["result-card","job-result-card","result-card--with-hover-state"])
-                for job in jobs:
-                    #to remove unwanted tags inbetween the text
-                    #[s.extract() for s in job(['style', 'script', '[document]', 'head', 'title'])]
-                    
-                    #no need of rating tab so we are gonna remove it too
-                    ratingContent=job.find(class_="ratingsContent")
-                    if ratingContent:
-                        ratingContent.extract()
+for url in urls:
+    with open("data_"+url["url_name"]+".csv","a") as dataFile:
+        writer=csv.writer(dataFile)
+        try:
+            print(url["url_name"]+" jobs")
+            for start in range(0,100,url["start_range"]):
+                html_doc=requests.get(url["url"].format(f1,f2,start),headers=headers).content
+                soup=BeautifulSoup(html_doc,'html.parser')
+                #to deal with encoding error
+                if(url['encoding']==True):
+                    with open("x.html","w",encoding="utf-8") as f:
+                        f.write(str(soup))
 
-                    #removing unwanted newlines ,whitespaces
-                    data=re.sub(r'[\r\n][\r\n]{1,}', '\n', job.text).strip().split("\n")[0:4]
+                    with open("x.html","r") as f:
+                        new_html=f.read()
+                        new_soup=BeautifulSoup(new_html,"html.parser")
+                        soup=new_soup        
 
-                    #adding the link attribute for easy redirection
-                    #link="https://www.indeed.co.in/jobs?q={}&l={}".format(data[0],data[2])
-                    #data.append(link)
+                #extracting the job blocks    
+                blocks=soup.find_all(url["class_tag"],class_=url["class"].split(' '))
 
-                    #appending the data in the csv file
-                    #writer.writerow(data)
-                    print(data)
+                for block in blocks:
+                    #print(block)
+                    title=block.select_one("[class*=title]").text.strip()
+                    company=(block.select_one("[class*=subtitle]") or block.select_one("[class*=company]")).text.strip()
+                    location=block.select_one("[class*=location]").text.strip()
+                    salary=block.select_one("[class*=salary]")
+                    link=block.select_one("a")['href']
 
-            #status checking        
-            print("page done{}".format(start))   
-            
-    except Exception as e:
-        print(e)            
+                    #url issues 
+                    if(url["url_name"] not in link):
+                        link="https://www."+url["url_name"]+".com"+link
+
+                    #if salary not present
+                    if(not salary == None):
+                        salary=salary.text.strip()
+                    else:
+                        salary=""
+                    data=[title,company,location,salary,link]
+                    writer.writerow(data)
+            print(url["url_name"]+"done")
+        except Exception as e:
+            print(e)
